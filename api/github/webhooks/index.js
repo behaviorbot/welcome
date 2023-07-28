8bd1c03cd535c4b39859e8beff07ff4125e982c5
Adam Yang <Adamyang2002@gmail.com>
2023-07-28 10:53:12 -0400

    Delete unnecessary files

--- api/github/webhooks/index.js
+++
@@ -1,6 +0,0 @@
-const { createNodeMiddleware, createProbot } = require("probot");
-
-const app = require("../../../app");
-const probot = createProbot();
-
-module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
